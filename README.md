035a7c6437e41ef0a4979ac677dcf2e25d190462
Mitsuhiro Inomata <inomata@tech-vein.com>
2016-02-24 20:28:48 +0900

    UUIDをランダム生成するようにした。

--- README.md
+++ README.md
@@ -3,7 +3,10 @@ OS X で BLEでなにかしたい
 
 
 ```
-# 初期化
+# 必要な環境をインストール
+brew install npm node
+npm install gulp-cli -g
+# npm アップデート
 npm install
 # src/ を監視・トランスパイルして dist/ にJS生成
 gulp
--- src/main.js
+++ src/main.js
@@ -1,20 +1,37 @@
 var Bleacon = require('bleacon');
 
-var uuid = '5124gafe3eadsaf241612333456fdd5a';
-// var uuid = 'e2c56db5dffb48d2b060d0f5a71096e0';
-var major = 0; // 0 - 65535
-var minor = 0; // 0 - 65535
-var measuredPower = -59; // -128 - 127 (measured RSSI at 1 meter)
-
-// Bleacon.startAdvertising(uuid, major, minor, measuredPower);
-// Bleacon.stopAdvertising();
-
+// http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function uuid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '' + s4() + '' + s4() + '' +
+    s4() + '' + s4() + s4() + s4();
+}
 
 Bleacon.on('discover', (bleacon)=>{
   console.log("on discover: ");
   console.log(bleacon);
 });
 
+
+var uuid = uuid(); // 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA';
+var major = 0;
+var minor = 0;
+var measuredPower = -59;
+
+console.log("current uuid is: "+uuid);
+console.log("Starting Advertising...");
+try {
+  Bleacon.startAdvertising(uuid, major, minor, measuredPower);
+} catch (e){
+  console.log("Failed to start advertising");
+  console.log(e);
+}
+
+console.log("Starting Scanning...");
 try {
   Bleacon.startScanning();
 } catch (e){
@@ -22,4 +39,4 @@ try {
   console.log(e);
 }
 
-
+console.log("Done.");
