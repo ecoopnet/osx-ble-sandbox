fc598a279aa36f0eeb769fa3febe39bc47c9760f
Mitsuhiro Inomata <inomata@tech-vein.com>
2016-02-24 19:47:11 +0900

    babel でes6変換してくれる環境を作った

---
+++ dist/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Foo = function Foo() {
+  _classCallCheck(this, Foo);
+};
+//# sourceMappingURL=all.js.map
